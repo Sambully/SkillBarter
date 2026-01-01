abf88835ff8d654c7272ac5347fbdc64089aa9ae
Samarth Pal <samarthpal1912005@gmail.com>
2026-01-02 01:27:27 +0530

    All done as a demo

--- Backend/index.js
+++ Backend/index.js
@@ -69,4 +69,5 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(5000, () => console.log("Backend running on port 5000"));
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
